bb346acab48560b9ac86b67cfa55633dfae0d70b
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2023-05-05 10:44:40 -0500

    FixedMoviesAvailable

--- src/app/components/cineclubs/movies-available/movies-available.component.ts
+++ src/app/components/cineclubs/movies-available/movies-available.component.ts
@@ -1,10 +1,89 @@
-import { Component,Input,OnInit } from '@angular/core';
+// import { Component,Input,OnInit } from '@angular/core';
+// import { ActivatedRoute } from '@angular/router';
+// import { MoviesProfileServiceService } from 'src/app/services/movies/movies-profile-service.service';
+// import { Schedule } from '../../../models/movie.model';
+// import { CineClub } from 'src/app/models/cineclub.model';
+// import { CineclubService } from 'src/app/services/cineclubs/cineclub.service';
+// import { switchMap ,map,tap} from 'rxjs/operators';
+
+// interface DateAvailable{
+//   id:number,
+//   day:string,
+//   hour: string[],
+//   cineclub:string
+// }
+
+// @Component({
+//   selector: 'cineclubs-movies-available',
+//   templateUrl: './movies-available.component.html',
+//   styleUrls: ['./movies-available.component.scss']
+// })
+// export class MoviesAvailableComponent implements OnInit {
+//   @Input() pos = 0;
+//   cineclub: any;
+//   dateAvailable: DateAvailable[] = [];
+
+//   constructor(
+//     private _servMoviesProfile: MoviesProfileServiceService,
+//     private _empServiceCineclub: CineclubService,
+//     private route : ActivatedRoute,
+//   ){}
+
+//   ngOnInit(): void {
+//     this.route.params.pipe(
+//       switchMap(params => {
+//         this.pos = params['id'];
+//         return this.getTitleCineclub();
+//       })
+//     ).subscribe(() => {
+//       this.getMovieAvailableHours().subscribe(() => {
+//         console.log('Date available:', this.dateAvailable);
+//       });
+//     });
+//   }
+
+//   getTitleCineclub() {
+//     return this._empServiceCineclub.getCineclubs().pipe(
+//       map(res => {
+//         this.cineclub = res[this.pos - 1].title;
+//         console.log(`1 ${this.cineclub}`);
+//       })
+//     );
+//   }
+
+
+//   getMovieAvailableHours() {
+//     return this._servMoviesProfile.getMovieListProfile().pipe(
+//       tap(res => {
+//         this.dateAvailable = [];
+//         for (let i = 0; i < res[this.pos].schedule.length; i++) {
+//           // console.log('iterating schedule', i, res[this.pos].schedule[i].cineclub, this.cineclub);
+//           if (res[this.pos].schedule[i].cineclub === this.cineclub) {
+//             this.dateAvailable.push(res[this.pos].schedule[i]);
+//             console.log(`2 ${this.dateAvailable.values}`);
+//           }
+//         }
+
+//       })
+//     );
+//   }
+
+// }
+
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MoviesProfileServiceService } from 'src/app/services/movies/movies-profile-service.service';
 import { Schedule } from '../../../models/movie.model';
 import { CineClub } from 'src/app/models/cineclub.model';
 import { CineclubService } from 'src/app/services/cineclubs/cineclub.service';
+import { switchMap, map, tap } from 'rxjs/operators';
 
+interface DateAvailable {
+  id: number;
+  day: string;
+  hour: string[];
+  cineclub: string;
+}
 
 @Component({
   selector: 'cineclubs-movies-available',
@@ -14,51 +93,50 @@ import { CineclubService } from 'src/app/services/cineclubs/cineclub.service';
 export class MoviesAvailableComponent implements OnInit {
   @Input() pos = 0;
   cineclub: any;
-  dateAvailable: Array<any> = [];
+  dateAvailable: string[] = [];
 
   constructor(
     private _servMoviesProfile: MoviesProfileServiceService,
     private _empServiceCineclub: CineclubService,
-    private route : ActivatedRoute,
-  ){}
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.pos = params['id'];    
-      //this.getTitleCineclub() 
+    this.route.params.pipe(
+      switchMap(params => {
+        this.pos = params['id'];
+        return this.getTitleCineclub();
+      })
+    ).subscribe(() => {
+      this.getMovieAvailableHours().subscribe((dateAvailable: string[]) => {
+        console.log('Date available:', dateAvailable);
+        this.dateAvailable = dateAvailable;
+      });
     });
-    this.getMovieAvailableHours()
   }
 
-  getTitleCineclub(){
-    this._empServiceCineclub.getCineclubs().subscribe({
-      next:(res) => {
-        this.cineclub = res[this.pos-1].title;
-        console.log(this.cineclub)
-      }      
-    })
+  getTitleCineclub() {
+    return this._empServiceCineclub.getCineclubs().pipe(
+      map(res => {
+        this.cineclub = res[this.pos - 1].title;
+        console.log(`1 ${this.cineclub}`);
+      })
+    );
   }
 
-  getMovieAvailableHours(){
-    this.getTitleCineclub();
-
-    this._servMoviesProfile.getMovieListProfile().subscribe({
-      next:(res) => {
-        //this.dateAvailable = res;
-        console.log(res[this.pos].schedule)
-
-        for(let i=0; i< res[this.pos].schedule.length; i++){
-          
-          if(res[this.pos].schedule[i].cineclub ===  this.cineclub){
-            this.dateAvailable.push(res[this.pos].schedule[i]) 
-            console.log(this.dateAvailable)
+  getMovieAvailableHours() {
+    return this._servMoviesProfile.getMovieListProfile().pipe(
+      map(res => {
+        const dateAvailable: string[] = [];
+        for (let i = 0; i < res[this.pos].schedule.length; i++) {
+          if (res[this.pos].schedule[i].cineclub === this.cineclub) {
+            dateAvailable.push(...res[this.pos].schedule[i].hour);
           }
         }
-        //this.dateAvailable[this.pos].schedule[0].cineclub
-      },
-      error: (err) => {
-        console.log(err)
-      }
-    })
+        console.log(`2 ${dateAvailable}`);
+        return dateAvailable;
+      })
+    );
   }
+
 }
