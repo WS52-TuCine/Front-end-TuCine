0fff633bcb363feff50d232c2e91645a39207d8c
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2023-05-04 19:49:38 -0500

    fixedRouting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,13 @@ import { FilmsComponent } from './components/films/films.component';
 import { ProfileComponent } from './components/movies/profile/profile.component';
 
 const routes: Routes = [
+  {path:'',pathMatch:'full',redirectTo:'explorar'},
+  { path: '', component: FilmsComponent },
   { path: 'peliculas', component: FilmsComponent },
-  { path: 'pelicula/:id', component: ProfileComponent }
+  { path: 'pelicula/:id', component: ProfileComponent },
+  {path:'**',pathMatch:'full',redirectTo:'home'},
+
+
 ];
 
 @NgModule({
