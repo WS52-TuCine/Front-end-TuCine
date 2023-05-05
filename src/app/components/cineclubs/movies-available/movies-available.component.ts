ad40666806671b6a4f8abfbc4dde4cfe66230e22
Tania Vásquez <103429138+taniasadith@users.noreply.github.com>
2023-05-05 10:06:48 -0500

    trying movie available

--- src/app/components/cineclubs/movies-available/movies-available.component.ts
+++ src/app/components/cineclubs/movies-available/movies-available.component.ts
@@ -20,35 +20,39 @@ export class MoviesAvailableComponent implements OnInit {
     private _servMoviesProfile: MoviesProfileServiceService,
     private _empServiceCineclub: CineclubService,
     private route : ActivatedRoute,
-  ){}
+  ){
 
-  ngOnInit(): void {
-    this.route.params.subscribe(params => {
-      this.pos = params['id'];    
-      //this.getTitleCineclub() 
-    });
-    this.getMovieAvailableHours()
   }
 
-  getTitleCineclub(){
+  async ngOnInit(): Promise <void> {
+    this.route.params.subscribe(params => {
+      this.pos = params['id'];       
+    });
+
+    await this.getTitleCineclub()
+    await this.getMovieAvailableHours()
+  }
+
+  async getTitleCineclub(){
     this._empServiceCineclub.getCineclubs().subscribe({
       next:(res) => {
         this.cineclub = res[this.pos-1].title;
-        console.log(this.cineclub)
+        console.log(`1 ${this.cineclub}`)
       }      
     })
   }
 
-  getMovieAvailableHours(){
-    this.getTitleCineclub();
+
+  async getMovieAvailableHours (){
+    //this.getTitleCineclub();
 
     this._servMoviesProfile.getMovieListProfile().subscribe({
       next:(res) => {
         //this.dateAvailable = res;
-        console.log(res[this.pos].schedule)
+        console.log( `2.${res[this.pos].schedule}`)
 
         for(let i=0; i< res[this.pos].schedule.length; i++){
-          
+
           if(res[this.pos].schedule[i].cineclub ===  this.cineclub){
             this.dateAvailable.push(res[this.pos].schedule[i]) 
             console.log(this.dateAvailable)
