35e558df8435f5f185a4cdf03471e69b84273be8
J_Arturo <73813395+pepper419@users.noreply.github.com>
2023-05-04 19:22:07 -0500

    firts version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './components/movies/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'pelicula/:id', component: ProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
