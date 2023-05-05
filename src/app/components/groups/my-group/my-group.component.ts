7344ae9c5f9ba2659bb9bcdd51464e36fc3bbbdf
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2023-05-05 06:56:31 -0500

    CardInProgress

--- src/app/components/groups/my-group/my-group.component.ts
+++ src/app/components/groups/my-group/my-group.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Group } from 'src/app/models/group.model';
+import { GroupService } from 'src/app/services/groups/group.service';
 
 @Component({
   selector: 'app-my-group',
@@ -6,5 +8,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./my-group.component.scss']
 })
 export class MyGroupComponent {
+
+  groups:Group[]=[]
+  groupsCopy:Group[]=[]
+
+  imageUrl = 'https://cinescopia.com/wp-content/uploads/2011/02/Posters-cine.jpg'; // URL de la imagen por defecto
+
+  constructor(private groupService:GroupService) {
+    this.groupService.getGroups().subscribe(res=>{
+      this.groups=res;
+      this.groupsCopy=res;
+      console.log(this.groups)
+    })
+  }
+
 
 }
