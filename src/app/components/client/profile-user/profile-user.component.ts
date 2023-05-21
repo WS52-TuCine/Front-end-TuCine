b7f4bdde6a485134dc968bee0d32678ffdd7dad7
YeniAlvarado <u20201b032@upc.edu.pe>
2023-05-21 18:57:43 -0500

    update
    profile

--- src/app/components/client/profile-user/profile-user.component.ts
+++ src/app/components/client/profile-user/profile-user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-profile-user',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile-user.component.scss']
 })
 export class ProfileUserComponent {
-
+  @Input() dataEntrante:any;
+  public image!: string;
 }
